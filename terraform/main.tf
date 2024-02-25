01ab021f0d83e1904f95bf11f96f64e6e7d7c22a
Y. volkovitz <yafiv@skyvar.co.il>
2024-02-25 15:40:19 +0200

    try

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,12 +15,12 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-data "azurerm_resource_group" "resource_group" {
+resource "azurerm_resource_group" "resource_group" {
   name     = var.rg_name
   location = var.rg_location
 }
 
-data "azurerm_storage_account" "storage_account" {
+resource "azurerm_storage_account" "storage_account" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = azurerm_resource_group.resource_group.location
