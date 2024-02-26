9cdfedffef18812e82fb8189afb77e7c50fa514a
Y. volkovitz <yafiv@skyvar.co.il>
2024-02-26 07:56:01 +0200

    try

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "NetworkWatcherRG"
-    storage_account_name = "myfirsttrail"
+    storage_account_name = "mysecondtrail"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
