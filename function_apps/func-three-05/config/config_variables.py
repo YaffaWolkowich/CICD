caaa64e531beef346219f84c193cac84253ecca8
Y. volkovitz <yafiv@skyvar.co.il>
2024-02-26 12:37:25 +0200

    func-05

--- function_apps/func-three-05/config/config_variables.py
+++ function_apps/func-three-05/config/config_variables.py
@@ -12,7 +12,8 @@ excel_secret_name = os.getenv("SECRET_EXCEL")
 credential = DefaultAzureCredential()
 client = SecretClient(vault_url=keyvault_uri, credential=credential)
 secret = client.get_secret(secret_name)
-connection_string = secret.value
+# connection_string = secret.value
+connection_string="DefaultEndpointsProtocol=https;AccountName=myfirsttrail;AccountKey=bz3aax2d3IX7b3ngkc73W+CqpzKRvUj4So1zAuUxlrpGDuzPDeOGT7tdwD4UiLzSu9iBO6jT6c69+AStETr8tg==;EndpointSuffix=core.windows.net"
 excel_secret = client.get_secret(excel_secret_name)
 excel_connection_string = excel_secret.value
 http_trigger_url = os.getenv("HTTP_TRIGGER_URL")
