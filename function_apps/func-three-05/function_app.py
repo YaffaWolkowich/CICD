965ed94334818d6003ea09e9455b0a9858cc2f42
Y. volkovitz <yafiv@skyvar.co.il>
2024-02-28 14:55:56 +0200

    try---

--- function_apps/func-three-05/function_app.py
+++ function_apps/func-three-05/function_app.py
@@ -27,10 +27,6 @@ def func_send_excel_mark_delete(req: func.HttpRequest) -> func.HttpResponse:
         partition_key = data["partition_key"]
         all_storages = data["all_storages"]
         write_and_upload(excel_connection_string, alerts_to_excel)
-        logging.warn("finish uploading111111111111111111111111111111111111111111111")
-    except Exception as e:
-        logging.warn(f"-<<->>-{e}")
-    try:
         requests.post(
             http_trigger_url,
             json={
@@ -40,9 +36,8 @@ def func_send_excel_mark_delete(req: func.HttpRequest) -> func.HttpResponse:
                 "excel": "alert_file.xlsx",
             },
         )
+        deleted_storages(documentation_table, int(partition_key) - 1, all_storages)
     except Exception as e:
-        logging.info("????????????????????<>><><><>>>>>>>>>>>>>>")
-        logging.warn(f"[[][][[][]]]   {e}")
-        # deleted_storages(documentation_table, int(partition_key) - 1, all_storages)
+        logging.warn(f"-<<->>-{e}")
 
     return func.HttpResponse("success - end logic app", status_code=200)
