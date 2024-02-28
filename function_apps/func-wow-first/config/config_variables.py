39bb96c5afc47292eb4805310260c19e49436cc2
Y. volkovitz <yafiv@skyvar.co.il>
2024-02-28 15:43:57 +0200

    try---

--- function_apps/func-wow-first/config/config_variables.py
+++ function_apps/func-wow-first/config/config_variables.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 email_secret = os.getenv("EMAILS_SECRET")
-# keyvault_name = os.getenv("KEYVAULT_NAME")
+keyvault_name = os.getenv("KEYVAULT_NAME")
 keyvault_uri = os.getenv("KEYVAULT_URI")
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 tenant_id = os.getenv("TENANT_ID")
-# user_upn = os.getenv("USER_UPN")
+user_upn = os.getenv("USER_UPN")
 graph_url = os.getenv("GRAPH_URL")
 
