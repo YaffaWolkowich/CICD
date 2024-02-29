32163d365ac2b3fdb0e4a23267d5106d2a8d1d8b
Y. volkovitz <yafiv@skyvar.co.il>
2024-02-29 17:21:36 +0200

    w

--- function_apps/func-three-05/function_app.py
+++ function_apps/func-three-05/function_app.py
@@ -9,8 +9,8 @@ import json
 #     main_manager,
 #     http_trigger_url,
 # )
-# from project.managed_deleted_storages import deleted_storages
-# from project.write_to_excel import write_and_upload
+from project.managed_deleted_storages import deleted_storages
+from project.write_to_excel import write_and_upload
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
