6046d07beabbfc5a1860b34c37b77b004e940791
Y. volkovitz <yafiv@skyvar.co.il>
2024-03-03 12:26:15 +0200

    Step on the way to the destination

--- services/storage/func-three-05/config/config_variables.py
+++ services/storage/func-three-05/config/config_variables.py
@@ -11,8 +11,8 @@ secret_name = os.getenv("SECRET")
 excel_secret_name = os.getenv("SECRET_EXCEL")
 credential = DefaultAzureCredential()
 client = SecretClient(vault_url=keyvault_uri, credential=credential)
-secret = client.get_secret(secret_name)
-connection_string = secret.value
+# secret = client.get_secret(secret_name)
+# connection_string = secret.value
 # excel_secret = client.get_secret(excel_secret_name)
 # excel_connection_string = excel_secret.value
 http_trigger_url = os.getenv("HTTP_TRIGGER_URL")
