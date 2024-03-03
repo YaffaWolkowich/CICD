7f32f855cf33ecaa421212bc0685370a054f635d
Y. volkovitz <yafiv@skyvar.co.il>
2024-03-03 13:12:02 +0200

    Step on the way to the destination

--- services/storage/func-three-05/project/write_to_excel.py
+++ services/storage/func-three-05/project/write_to_excel.py
@@ -1,12 +1,15 @@
 from azure.storage.blob import BlobServiceClient
 import pandas as pd
 import os
-
+import  logging
 def write_and_upload(connection_string, data):
+    logging.warn("1. write")
     blob_file = "alert_file.xlsx"
     container = "excel"
     write_excel_file(data, blob_file)
+    logging.warn("2. write")
     upload_blob_file(connection_string, container, blob_file)
+    logging.warn("3. write")
 
 
 def write_excel_file(data, blob_file):
@@ -18,11 +21,15 @@ def write_excel_file(data, blob_file):
         "Subscription Manager Email",
     ]
     df.to_excel(blob_file, index=False, header=custom_header)
+    logging.info("---------------------------")
 
 
 def upload_blob_file(connection_string, container, blob_file):
-
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    container_client = blob_service_client.get_container_client(container=container)
-    with open(os.path.join("./", blob_file), "rb") as data:
-        container_client.upload_blob(name=blob_file, data=data, overwrite=True)
+    try:
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        container_client = blob_service_client.get_container_client(container=container)
+        with open(os.path.join("./", blob_file), "rb") as data:
+            container_client.upload_blob(name=blob_file, data=data, overwrite=True)
+    except Exception as e:
+        logging.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        logging.warn(f"<><><><><><><><><>{e}")
