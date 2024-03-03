b9904535e74e0b4bc3e2e78364f5d5df9f9929d0
Y. volkovitz <yafiv@skyvar.co.il>
2024-03-03 12:40:54 +0200

    Step on the way to the destination

--- services/storage/func-three-05/function_app.py
+++ services/storage/func-three-05/function_app.py
@@ -7,12 +7,12 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-# from config.config_variables import (
-#     documentation_table,
-#     excel_connection_string,
-#     main_manager,
-#     http_trigger_url,
-# )
+from config.config_variables import (
+    documentation_table,
+    excel_connection_string,
+    main_manager,
+    http_trigger_url,
+)
 import config.config_variables
 
 from project.managed_deleted_storages import deleted_storages
@@ -31,20 +31,20 @@ def func_send_excel_mark_delete(req: func.HttpRequest) -> func.HttpResponse:
         data = json.loads(my_json)
         logging.info(f"data: {data}")
         logging.warn(config.config_variables.main_manager)
-        # alerts_to_excel = data["alerts_to_excel"]
-        # partition_key = data["partition_key"]
-        # all_storages = data["all_storages"]
-        # write_and_upload(excel_connection_string, alerts_to_excel)
-        # requests.post(
-        #     http_trigger_url,
-        #     json={
-        #         "recipient_email": main_manager,
-        #         "subject": "Summary Alerts For Storage Accounts",
-        #         "body": "summary file",
-        #         "excel": "alert_file.xlsx",
-        #     },
-        # )
-        # deleted_storages(documentation_table, int(partition_key) - 1, all_storages)
+        alerts_to_excel = data["alerts_to_excel"]
+        partition_key = data["partition_key"]
+        all_storages = data["all_storages"]
+        write_and_upload(excel_connection_string, alerts_to_excel)
+        requests.post(
+            http_trigger_url,
+            json={
+                "recipient_email": main_manager,
+                "subject": "Summary Alerts For Storage Accounts",
+                "body": "summary file",
+                "excel": "alert_file.xlsx",
+            },
+        )
+        deleted_storages(documentation_table, int(partition_key) - 1, all_storages)
     except Exception as e:
         logging.warn(f"-<<->>-{e}")
 
