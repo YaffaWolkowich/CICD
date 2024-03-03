5c15913165606754b8a5f5665d0b8a937b837c6c
Y. volkovitz <yafiv@skyvar.co.il>
2024-03-03 15:49:31 +0200

    Step on the way to the destination

--- services/storage/func-three-05/function_app.py
+++ services/storage/func-three-05/function_app.py
@@ -3,9 +3,9 @@ import logging
 import requests
 import json
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from config.config_variables import (
     documentation_table,
